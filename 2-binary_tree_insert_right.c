4bc7c1543eb72a8046368219471e9c95a18c384f
kenialo19 <3396@holbertonschool.com>
2021-12-01 18:00:32 -0500

    adding error case if parent fails

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,18 +11,21 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (!parent)
+		return (NULL);
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
-        {
-                return (NULL);
-        }
+	{
+		return (NULL);
+	}
 
-		new_node->n = value;
-		new_node->parent = parent;
-		new_node->left = NULL;
-		new_node->right = parent->right;
-		parent->right = new_node;
-		if (new_node->right)
-			new_node->right->parent = new_node;
-		return (new_node);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = parent->right;
+	parent->right = new_node;
+	if (new_node->right)
+		new_node->right->parent = new_node;
+	return (new_node);
 }
