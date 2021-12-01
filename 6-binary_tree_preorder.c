65e2d26224b7c1c4493254e3e9cfef1d14635c0c
kenialo19 <3396@holbertonschool.com>
2021-12-01 18:03:06 -0500

    delete space of the line 14

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - Function that goes through a binary tree using pre-order traversal.
+ * binary_tree_preorder - Function that goes through
+ * a binary tree using pre-order traversal.
  * @tree: Pointer to the root node of the tree to traverse.
  * @func: Pointer to a function to call for each node.
  *
@@ -10,7 +11,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
-    
+
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
