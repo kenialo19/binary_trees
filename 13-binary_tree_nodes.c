f14b6ec74454ab4047f1beed58640772c8aaa850
JessicaJulieth <3497@holbertonschool.com>
2021-12-01 16:35:33 -0600

    Add task 13 with betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,8 +14,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if ((tree->left == NULL && tree->right != NULL) ||
-		 (tree->left != NULL && tree->right == NULL))
-			return (1);
+		(tree->left != NULL && tree->right == NULL))
+		return (1);
 
 	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 }
